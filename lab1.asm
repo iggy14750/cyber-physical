0e98a34391400f351cb49669e8b049edbbce1c5b
Isaac <isaacgoss14750@gmail.com>
2018-01-19 14:40:30 -0500

    Turns out you have to spell OR and XOR differently than I believed.

--- lab1.asm
+++ lab1.asm
@@ -7,8 +7,8 @@
     .thumbfunc main
 
 main: .asmfunc
-    MOV  R1, #0x4C00    ; Address of Buttons, low 16 bits
-    MOVT R1, #0x4000    ; Address of Buttons, top 16 bits
+    mov  R1, #0x4C00    ; Address of Buttons, low 16 bits
+    movt R1, #0x4000    ; Address of Buttons, top 16 bits
 
     ADD  R2, R1, #3     ; Address of LEDs
 
@@ -19,16 +19,16 @@ Top:                    ; Our read loop.
     AND  R4, R3, #1     ; R4 is the state of button 2
     LSR  R3, R3, #3     ; R3 is the state of button 1
 
-    OR   R5, R3, R4     ; BLUE = not (b1 or b2), still need 'not'.
-    XOR  R5, R5, #1     ; BLUE = not (b1 or b2), here we invert that bit.
+    ORR  R5, R3, R4     ; BLUE = not (b1 or b2), still need 'not'.
+    EOR  R5, R5, #1     ; BLUE = not (b1 or b2), here we invert that bit.
 
     LSL  R5, R5, #1     ; Need to pack RED and GREEN in lower bits.
     AND  R6, R3, R4     ; GREEN = b1 and b2.
-    OR   R5, R5, R6     ; R5 = (BLUE << 1) | GREEN
+    ORR  R5, R5, R6     ; R5 = (BLUE << 1) | GREEN
 
     LSL  R5, R5, #1     ; Still need to pack RED
-    XOR  R6, R3, R4     ; RED = b1 xor b2
-    OR   R5, R5, R6     ; R5 = (((BLUE << 1) | GREEN) << 1) | RED
+    EOR  R6, R3, R4     ; RED = b1 xor b2
+    ORR  R5, R5, R6     ; R5 = (((BLUE << 1) | GREEN) << 1) | RED
 
     STRB R5, [R2]       ; Write our calculations to LEDs.
 
