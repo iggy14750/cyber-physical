21a513857fabd1ac275a3d74a0f8dc8cb9cd1731
iggy14750 <ibg4@pitt.edu>
2018-01-24 18:40:34 -0500

    initializion compiles!

--- DigitalLock.c
+++ DigitalLock.c
@@ -1,35 +1,39 @@
 #include <stdint.h>
 #include "msp432p401r.h"
 
+#define P1_BTIS 0x12    /* Bit1 = Button2, Bit4 = Button1 */
+#define P2_BITS 0x7     /* Bit0 = RED, Bit1 = GREEN, Bit2 = BLUE */
 
 void Initialize_Port1(void)
 {
-    
+    P1SEL0 = 0;
+    P1SEL1 = 0;
+    P1REN  = P1_BTIS;
 }
 
 void Initialize_Port2(void)
 {
-    P2SEL0 = 0;     // Select to 0
-    P2SEL1 = 0;     // Select to 0
-    P2DIR = 0x12;   // Direction, bits 4 and 1 to OUT
-    P2REN = 0x12;   // Resistor enable, same bits.
+    P2SEL0 = 0;
+    P2SEL1 = 0;
+    P2DIR  = P2_BITS;
+    P2REN  = P2_BITS;
 }
 
 
 uint8_t Read_Port1(void)
 {
-
+    return P1IN;
 }
 
 
 
 void Write_Port2(uint8_t value)
 {
-
+    P2OUT = value;
 }
 
 
 int main(void)
 {
-
+    return 0;
 }
