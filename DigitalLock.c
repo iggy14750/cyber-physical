100491eec5cae5956d0aae6c6cd5d96d8035d20a
iggy14750 <ibg4@pitt.edu>
2018-01-24 18:19:37 -0500

    Initializing port 2

--- DigitalLock.c
+++ DigitalLock.c
@@ -4,12 +4,15 @@
 
 void Initialize_Port1(void)
 {
-
+    
 }
 
 void Initialize_Port2(void)
 {
-
+    P2SEL0 = 0;     // Select to 0
+    P2SEL1 = 0;     // Select to 0
+    P2DIR = 0x12;   // Direction, bits 4 and 1 to OUT
+    P2REN = 0x12;   // Resistor enable, same bits.
 }
 
 
