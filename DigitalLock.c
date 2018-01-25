db26c14e13d703c4bdf99e15ea1f2789a8e161d4
iggy14750 <ibg4@pitt.edu>
2018-01-24 19:29:40 -0500

    lab2 part A done.

--- DigitalLock.c
+++ DigitalLock.c
@@ -3,6 +3,20 @@
 
 #define P1_BTIS 0x12    /* Bit1 = Button2, Bit4 = Button1 */
 #define P2_BITS 0x7     /* Bit0 = RED, Bit1 = GREEN, Bit2 = BLUE */
+#define BUTTON1(x) (uint8_t) (((x) & 0x10) >> 4)
+#define BUTTON2(x) (uint8_t) (((x) & 0x02) >> 1)
+#define PACK(blue, green, red) (uint8_t) (((blue) << 2) | ((green) << 1) | (red))
+
+uint8_t convert_button_to_LED(uint8_t buttons)
+{
+    uint8_t one = BUTTON1(buttons);
+    uint8_t two = BUTTON2(buttons);
+    return PACK(
+        one & two,      // BLUE = b1 and b2
+        (one | two) ^ 1,// GREEN = not (b1 or b2)
+        one ^ two       // RED = b1 xor b2
+    );
+}
 
 void Initialize_Port1(void)
 {
@@ -31,5 +45,9 @@ void Write_Port2(uint8_t value)
 
 int main(void)
 {
+    while (1)
+    {
+        Write_Port2(convert_button_to_LED(Read_Port1()));
+    }
     return 0;
 }
